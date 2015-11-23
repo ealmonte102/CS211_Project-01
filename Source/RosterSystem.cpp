d8cdecbfbe55548e9d8c230467c98fb30571e5ba
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-11-23 15:51:55 -0500

    Added declaration for operator<< overload. Copy constructor and assignment operator left undefined.

--- Source/RosterSystem.cpp
+++ Source/RosterSystem.cpp
@@ -2,3 +2,27 @@
 // Created by Evan Almonte
 //
 #include "RosterSystem.hpp"
+#include "Roster.hpp"
+
+
+RosterSystem::RosterSystem( ) : loginStatus(NOT_LOGGED), size(0),
+                                capacity(0), rosterList(new Roster*[capacity]) {
+	rosterList = nullptr;
+}
+
+RosterSystem::~RosterSystem( ) {
+	for (int i = 0; i < size; ) {
+		delete rosterList[i];
+	}
+	delete[] rosterList;
+}
+
+void RosterSystem::grow( ) {
+	int newCap = capacity * 2 + 1;
+	Roster** tempList = new Roster*[newCap];
+	for (int i = 0; i < size; ++i) {
+		tempList[i] = rosterList[i];
+	}
+	delete[] rosterList;
+	rosterList = tempList;
+}
--- Source/RosterSystem.hpp
+++ Source/RosterSystem.hpp
@@ -1,23 +1,39 @@
 ﻿#ifndef RosterSystem_HPP
 #define RosterSystem_HPP
 
-class Roster;
+#include <iosfwd>
+#include <string>
+#include "Roster.hpp"
+
 class RosterSystem {
 public:
 	RosterSystem ( );
 	
 	~RosterSystem ( );
 
+	friend std::ostream& operator<<(std::ostream& output, const RosterSystem& rhs);
+
+	void selectRoster ( );
+
 private:
-	RosterSystem (const RosterSystem& rhs);
+	RosterSystem (const RosterSystem& rhs) = delete;
 	
-	RosterSystem& operator=(const RosterSystem& rhs);
+	RosterSystem& operator=(const RosterSystem& rhs) = delete;
+
+	void grow ( );
+
+	void addRoster ();
+
+	void removeRoster (std::string courseNumber);
 
 	enum loginFlag {
 		NOT_LOGGED, USER, SUPERVISOR
 	} loginStatus;
 	int size;
-	Roster* rosterList;
+	int capacity;
+	Roster** rosterList;
+
+
 };
 
 #endif //RosterSystem_HPP
