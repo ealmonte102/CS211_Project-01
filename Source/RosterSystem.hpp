c98a715747be89a3049ee90cec760d92f7135816
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-11-23 11:56:51 -0500

    Initial member functions for the RosterSystem class created in the .hpp file.

--- Source/RosterSystem.hpp
+++ Source/RosterSystem.hpp
@@ -1,11 +1,23 @@
 ﻿#ifndef RosterSystem_HPP
 #define RosterSystem_HPP
 
+class Roster;
 class RosterSystem {
 public:
+	RosterSystem ( );
 	
-private:
+	~RosterSystem ( );
 
+private:
+	RosterSystem (const RosterSystem& rhs);
+	
+	RosterSystem& operator=(const RosterSystem& rhs);
+
+	enum loginFlag {
+		NOT_LOGGED, USER, SUPERVISOR
+	} loginStatus;
+	int size;
+	Roster* rosterList;
 };
 
 #endif //RosterSystem_HPP
