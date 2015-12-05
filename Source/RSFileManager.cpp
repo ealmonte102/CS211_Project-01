0100564ff860d82b7ad5d7e336ac903ab7cc6377
Evan Almonte <ealmonte102@qc.cuny.edu>
2015-12-05 14:22:54 -0500

    Adds class RSFileManager which handles exports and imports

---
+++ Source/RSFileManager.cpp
@@ -0,0 +1,84 @@
+﻿// RSFileManager.cpp
+// Created by Evan Almonte
+//
+#include "RSFileManager.hpp"
+#include "Student.hpp"
+#include "Utilities.hpp"
+#include <iostream>
+#include <string>
+
+using std::string;
+using std::cout;
+using std::cin;
+
+RosterSystem::RSFileManager::RSFileManager(std::string fileName, bool useAsOutput) : fileName(fileName) {
+	if (useAsOutput) {
+		ioFile.open(fileName.c_str(), std::ios::out);
+	} else {
+		ioFile.open(fileName.c_str(), std::ios::in);
+	}
+}
+
+bool RosterSystem::RSFileManager::loginIsValid(std::string username, std::string password) {
+	if (ioFile.is_open()) {
+		string uN, pN;
+		while (!ioFile.eof()) {
+			ioFile >> uN >> pN;
+			if (upperConvert(username) == upperConvert(uN) && password == pN) {
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+RosterSystem::loginFlags RosterSystem::RSFileManager::attemptLogin( ) {
+	string username;
+	string password;
+	int numOfTries = 3;
+	while (numOfTries-- > 0) {
+		cout << "Please enter your username: ";
+		getline(cin, username);
+		cout << "Please enter your password: ";
+		getline(cin, password);
+		if (!ioFile.is_open()) {
+			ioFile.open(fileName.c_str());
+		}
+		if (loginIsValid(username, password)) {
+			return SUPERVISOR;
+		}
+		ioFile.close();
+		cout << "Invalid login. Please try again " << numOfTries << " tries left.\n" ;
+	}
+	return USER;
+}
+
+void RosterSystem::RSFileManager::exportRosters(const Roster* const* const rosterList, int size) {
+	if (ioFile.is_open()) {
+		for (int i = 0; i < size; ++i) {
+			const Roster* currentRoster = rosterList[i];
+			ioFile << rosterList[i]->getCourseName() << " | ";
+			ioFile << rosterList[i]->getCourseCode() << " | ";
+			ioFile << rosterList[i]->getNumOfCredits() << " | ";
+			ioFile << rosterList[i]->getInstructorName() << "\n";
+			int numOfStudents = rosterList[i]->getNumEnrolled();
+			for (int j = 0; j < numOfStudents; ++j) {
+				const Student currentStudent = (*currentRoster)[j];
+				ioFile << currentStudent.getFirstName() << " | ";
+				ioFile << currentStudent.getLastName() << " |";
+				ioFile << currentStudent.getId() << "|";
+				ioFile << currentStudent.getStanding() << "|";
+				ioFile << currentStudent.getCredits() << "|";
+				ioFile << currentStudent.getGpa() << "|";
+				ioFile << currentStudent.getDob().getDay() << "/";
+				ioFile << currentStudent.getDob().getMonthNum() << "/";
+				ioFile << currentStudent.getDob().getYear() << "|";
+				ioFile << currentStudent.getDob().getDay() << "/";
+				ioFile << currentStudent.getDob().getMonthNum() << "/";
+				ioFile << currentStudent.getDob().getYear() << "\n";
+			}
+			ioFile << "end_roster|";
+			if (i != size - 1) { ioFile << "\n"; }
+		}
+	}
+}
---
+++ Source/RSFileManager.hpp
@@ -0,0 +1,21 @@
+﻿#ifndef RSFileManager_HPP
+#define RSFileManager_HPP
+#include "RosterSystem.hpp"
+#include <fstream>
+#include <string>
+
+class RosterSystem::RSFileManager {
+public:
+	RSFileManager(std::string fileName, bool useAsOutput);
+	bool loginIsValid(std::string username, std::string password);
+	loginFlags attemptLogin( );
+	void exportRosters(const Roster* const* const rosterList, int size);
+	void importRosters( ) = delete;
+private:
+	std::string fileName;
+	std::fstream ioFile;
+};
+
+#endif //RSFileManager_HPP
+
+
